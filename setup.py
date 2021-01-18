9d94f69d5ab5bd5a1c6108e3b27d34cbda3ddaaf
Tishka17 <tishka17@mail.ru>
2021-01-18 19:07:12 +0300

    fix importing models

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='reclada.devops.domino',
     description='Proper python bindings for the Domino API.',
     version='0.1',
-    packages=['reclada.devops.domino'],
+    packages=['reclada.devops.domino', 'reclada.devops.domino.models'],
     install_requires=[
         'requests',
         'dataclass_factory==2.*',
